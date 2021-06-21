51e5458dec2ae15d1932dba1ab786248d54a0e12
rikutoiri <83803966+rikutoiri@users.noreply.github.com>
2021-06-21 14:18:59 +0900

    Add files via upload

---
+++ Stage.h
@@ -0,0 +1,17 @@
+//#pragma once
+//
+//#define DEF_Stage_H
+//
+//typedef struct {
+//	int Handle_stage;	//•Ç‚Ì‰æ‘œ‚ÌŠi”[—p•Ï”(‰æ‘œ‚Ì“Ç‚İ‚İ—p•Ï”)
+//	int Stage[12][21];		//Stageî•ñ‚ÌŠi”[—p•Ï”
+//} Stage_t;
+//
+//// ‰Šú‰»‚ğ‚·‚é
+//void Stage_Initialize();
+//
+//// •`‰æ‚·‚é
+//void Stage_Draw();
+//
+//// I—¹ˆ—‚ğ‚·‚é
+//void Stage_Finalize();
